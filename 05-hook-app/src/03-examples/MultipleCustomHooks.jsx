a7321c0ebd22827ca01e3abfadba484f2924ec42
Jenner Acosta Díaz <my@jenner.pe>
2022-11-04 06:58:53 -0500

    useFetch + useCounter

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -3,11 +3,30 @@ import { useFetch } from "../Hooks/useFetch"
 export const MultipleCustomHooks = () => {
 
     const { data, isLoading, hasError } = useFetch('https://www.breakingbadapi.com/api/quotes/1');
-    console.log(data, isLoading, hasError);
+    
+    
+    const { author, quote } = !!data && data[0];
 
     return (
         <>
             <h1>BrakingBad Quotes</h1>
+            <hr />
+
+            {
+                (isLoading)
+                    ? (
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+                    )
+                    : (
+                        <blockquote className="blockquote text-end">
+                            <p className="mb-2">{quote}</p>
+                            <footer className="blockquote-footer">{author}</footer>
+                        </blockquote>
+                    )
+            }
+            <button className="btn btn-primary">Next quote</button>
         </>
     )
 }
