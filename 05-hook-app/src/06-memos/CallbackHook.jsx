2b5f51c6d380bf3e418713945abe9f807c3e3986
Jenner Acosta Díaz <my@jenner.pe>
2022-11-04 10:42:39 -0500

    useCallback con argumentos

--- 05-hook-app/src/06-memos/CallbackHook.jsx
+++ 05-hook-app/src/06-memos/CallbackHook.jsx
@@ -6,8 +6,8 @@ export const CallbackHook = () => {
     const [counter, setCounter] = useState(10);
 
     const incrementFather = useCallback(
-      () => {
-        setCounter( (value) => value + 1 );
+      (value) => {
+        setCounter( (c) => c + value );
       },
       [],
     )
--- 05-hook-app/src/06-memos/ShowIncrement.jsx
+++ 05-hook-app/src/06-memos/ShowIncrement.jsx
@@ -8,7 +8,7 @@ export const ShowIncrement = ({ increment }) => {
         <button
             className="btn btn-primary"
             onClick={() => {{
-                increment();
+                increment(5);
             }}}
         >
             Incrementar
