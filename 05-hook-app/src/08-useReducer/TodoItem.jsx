a19cf900a35115212fd50fe15cf49371d0121fdd
Jenner Acosta Díaz <my@jenner.pe>
2022-11-05 17:45:03 -0500

    Finalizando TODO

--- 05-hook-app/src/08-useReducer/TodoItem.jsx
+++ 05-hook-app/src/08-useReducer/TodoItem.jsx
@@ -1,6 +1,6 @@
 export const TodoItem = ( { todo, onDeleteTodo, onToggleTodo } ) => {
     return (
-        <li className={`list-group-item d-flex justify-content-between ${ todo.done && 'list-group-item-action list-group-item-success'}`}>
+        <li className={`list-group-item d-flex justify-content-between ${ todo.done && ' list-group-item-success'}`}>
             <span 
                 className={`align-self-center ${ todo.done && 'text-decoration-line-through'}`}
                 onClick={ () => onToggleTodo ( todo.id ) }
