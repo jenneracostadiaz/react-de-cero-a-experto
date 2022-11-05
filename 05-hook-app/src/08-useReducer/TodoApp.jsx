1deb24b065b5f428eca6933ae1a5554243784fa8
Jenner Acosta Díaz <my@jenner.pe>
2022-11-05 15:38:32 -0500

    Creando el cascarón de la lista de TODOs

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -20,13 +20,35 @@ export const TodoApp = () => {
 
     return (
         <>
-            <div>TodoApp</div>
+            <h1>TodoApp 10, <small>pendientes: 2</small></h1>
             <hr />
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group">
+                        {
+                            todos.map( todo => {
+                                <li key={ todo.id } className="list-group-item d-flex justify-content-between">
+                                    <span className="align-self-center">Item 1</span>
+                                    <button className="btn btn-danger">Borrar</button>
+                                </li>
+                            })
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    <form action="">
+                        <input 
+                            type="text"
+                            placeholder="Nombre tarea"
+                            className="form-control"
+                        />
+                        <button type="submit" className="btn btn-outline-primary mt-2">Agrergar</button>
+                    </form>
+                </div>
+            </div>
+            
         </>
     )
 }
