dc6767e70ffbd06c291d1390560761622b1afa78
Jenner Acosta Díaz <my@jenner.pe>
2022-11-05 23:05:24 -0500

    Link

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,13 +1,16 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, Link } from "react-router-dom";
 import { AboutPage } from "./AboutPage";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
+import { Navbar } from "./Navbar";
 
 export const MainApp = () => {
   return (
     <>
         <h1>MainApp</h1>
         <hr />
+
+        <Navbar />
 
         <Routes>
           <Route path="/" element={ <HomePage /> } />
