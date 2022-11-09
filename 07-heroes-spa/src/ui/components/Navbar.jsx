33d01cc51dfa9e438bab6442e3cf54d82a58b245
Jenner Acosta Díaz <my@jenner.pe>
2022-11-09 11:13:28 -0500

    Navigate push / replace - useNavigate

--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-3" >
             
@@ -50,6 +59,7 @@ export const Navbar = () => {
                     </span>
                     <button
                         className='nav-item nav-link btn'
+                        onClick={ onLogout }
                     >
                         Logout
                     </button>
