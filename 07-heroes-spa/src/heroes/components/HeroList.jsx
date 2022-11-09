d8412ebce9ec12ff299f4101411e49e82a965b7f
Jenner Acosta Díaz <my@jenner.pe>
2022-11-09 14:59:29 -0500

    UseMemo

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { getHeroesByPublisher } from '../helpers'
 import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher( publisher );
+    const heroes = useMemo(() => getHeroesByPublisher( publisher ), [ publisher ]);
 
     return (
         <div className='row row-cols-1 row-cols-sm-3 g-3'>
--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -43,12 +43,6 @@ export const Navbar = () => {
                     >
                         Search
                     </NavLink>
-                    <NavLink
-                        className={ ({isActive}) => `nav-item nav-link ${ isActive ? 'active' : '' }` }
-                        to="/hero"
-                    >
-                        Hero
-                    </NavLink>
                 </div>
             </div>
 
