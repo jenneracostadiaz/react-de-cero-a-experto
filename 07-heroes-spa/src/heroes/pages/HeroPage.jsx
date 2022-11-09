f05a44e55e736174d76756c084d3edef6aec479a
Jenner Acosta Díaz <my@jenner.pe>
2022-11-09 14:54:31 -0500

    Estilo del componente HeroScreen

--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
@@ -6,12 +6,36 @@ export const HeroPage = () => {
   const { id } = useParams();
 
   const hero = getHeroById(id);
+
+  const navigate = useNavigate();
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
   
   if ( !hero ){
     return <Navigate to="/marvel" />
   }
 
   return (
-    <h1>{hero.superhero}</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={ `/assets/heroes//${hero.id}.jpg`} alt={hero.superhero} className="img-thumbnail" />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego:</b> {hero.alter_ego}</li>
+          <li className="list-group-item"><b>Publisher:</b> {hero.publisher}</li>
+          <li className="list-group-item"><b>First Appearance:</b> {hero.first_appearance}</li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+        <button
+          className="btn btn-primary"
+          onClick={ onNavigateBack }
+        >Back</button>
+      </div>
+    </div>
   )
 }
