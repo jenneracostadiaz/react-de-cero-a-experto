c16679c92f71789cfe39c598a7f73a7d259d4826
Jenner Acosta Díaz <my@jenner.pe>
2022-11-09 15:13:22 -0500

    Diseños de Pagina Search

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,40 @@
+import { HeroCard } from "../components/HeroCard";
+
 export const SearchPage = () => {
   return (
-    <h1>SearchPage</h1>
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching:</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+            <button
+              className="btn btn-primary mt-2">
+              Search
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+          <div className="alert alert-danger">
+            No hero with <b>ABC</b>
+          </div>
+          {/* <HeroCard /> */}
+        </div>
+      </div>
+    </>
   )
 }
