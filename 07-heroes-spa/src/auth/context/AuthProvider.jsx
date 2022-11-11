61de68ec2602eb8e73aebe458c043fd82efbbf6a
Jenner Acosta Díaz <my@jenner.pe>
2022-11-11 18:30:26 -0500

    Mantener al usuario activo

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -7,11 +7,22 @@ const initialState = {
     logged: false,
 }
 
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return {
+        logged: !!user,
+        user: user,
+    }
+}
+
 export const AuthProvider = ({ children }) => {
 
-    const [ authState, dispatch ] = useReducer( authReducer, initialState)
+    const [ authState, dispatch ] = useReducer( authReducer, initialState, init)
 
     const login = ( name = '') => {
+
+        const user = { id: 'ABC', name}
+
         const action = {
              type: types.login,
              payload: {
@@ -19,6 +30,9 @@ export const AuthProvider = ({ children }) => {
                 name: name
              }
         }
+
+        localStorage.setItem('user', JSON.stringify( user ))
+
         dispatch(action)
     }
 
