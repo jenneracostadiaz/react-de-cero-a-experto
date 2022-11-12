82cefb2df1fcaf534d447f54d5bf9add16792994
Jenner Acosta Díaz <my@jenner.pe>
2022-11-11 21:01:26 -0500

    Rutas privadas

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -3,13 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import { Navbar } from "../ui";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
           <Route path="login" element={<LoginPage />} />
-          <Route path="/*" element={<HeroesRoutes />} />    
+          <Route path="/*" element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          } />
+          {/* <Route path="/*" element={<HeroesRoutes />} />     */}
       </Routes>
     </>
   )
