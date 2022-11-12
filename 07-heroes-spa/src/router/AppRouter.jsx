6bae0b6233266387c6cbeca60ecf1f49c61ea257
Jenner Acosta Díaz <my@jenner.pe>
2022-11-11 21:12:26 -0500

    Rutas Públicas

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -4,12 +4,18 @@ import { Navbar } from "../ui";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-          <Route path="login" element={<LoginPage />} />
+          {/* <Route path="login" element={<LoginPage />} /> */}
+          <Route path="login" element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          } />
           <Route path="/*" element={
             <PrivateRoute>
               <HeroesRoutes />
