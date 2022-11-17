d99a138db6bcde353443898724afddf0fd5479e7
Jenner Acosta Díaz <my@jenner.pe>
2022-11-17 18:46:04 -0500

    Boton Flotante

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
@@ -5,10 +7,24 @@ export const JournalPage = () => {
 	return (
 		<JournalLayout>
 			{/* NothingSelected */}
-			{/* <NothingSelectedView /> */}
+			<NothingSelectedView />
 
 			{/* NoteView */}
-			<NoteView />
+			{/* <NoteView /> */}
+
+			<IconButton
+				size="large"
+				sx={{
+					color: 'white',
+					backgroundColor: 'error.main',
+					':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+					position: 'fixed',
+					right: 50,
+					bottom: 50,
+				}}
+			>
+				<AddOutlined sx={{ fontSize: 30 }} />
+			</IconButton>
 		</JournalLayout>
 	);
 };
