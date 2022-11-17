d0dffe076f8d55f68e1a48ccb86a47b405fa316a
Jenner Acosta Díaz <my@jenner.pe>
2022-11-17 17:42:08 -0500

    NavBar

--- 08-journal-app/src/journal/layout/JournalLayout.jsx
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/system';
 import React from 'react';
+import { NavBar } from '../components';
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
 	return (
 		<Box sx={{ display: 'flex' }}>
+			<NavBar drawerWidth={drawerWidth} />
 			{/* Navbar */}
 			{/* Sibebar */}
 			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
