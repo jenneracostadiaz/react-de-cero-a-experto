86e9f5889f0cf7e28d68c6f0ef106f947c71bcc0
Jenner Acosta Díaz <my@jenner.pe>
2022-11-17 18:20:09 -0500

    NothingSelectedView - No hay nada seleccionado

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,15 +1,10 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
 	return (
 		<JournalLayout>
-			<Typography>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio
-				in incidunt praesentium provident quod quos doloremque cum
-				blanditiis eius at hic distinctio, excepturi voluptatem
-				dolorem dignissimos aspernatur eum expedita similique.
-			</Typography>
+			<NothingSelectedView />
 			{/* NothingSelected */}
 			{/* NoteView */}
 		</JournalLayout>
