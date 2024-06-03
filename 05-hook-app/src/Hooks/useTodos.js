2c2287b59c554f2efd01153ab425642b5a9e8cb7
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2024-06-03 14:45:49 -0500

    useReducer

--- 05-hook-app/src/Hooks/useTodos.js
+++ 05-hook-app/src/Hooks/useTodos.js
@@ -1,48 +1,45 @@
-import { useEffect, useReducer } from "react";
-import { todoReducer } from "../08-useReducer/todoReducer";
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
 
 // const initialState = [];
 
 const init = () => {
-    return JSON.parse( localStorage.getItem('todos')) || [];
-}
+	return JSON.parse(localStorage.getItem('todos')) || [];
+};
 
 export const useTodos = () => {
-    const [todos, dispatch] = useReducer( todoReducer, [], init);
+	const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify( todos ));
-    }, [todos])
-    
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
 
-    const handleNewTodo = ( todo ) => {
-        const action = {
-            type: '[TODO] Add todo',
-            payload: todo
-        }
-        dispatch( action );
-    }
+	const handleNewTodo = (todo) => {
+		const action = {
+			type: '[TODO] Add todo',
+			payload: todo,
+		};
+		dispatch(action);
+	};
 
-    const handleDeleteTodo = ( id ) => {
-        // console.log({id});
-        dispatch({
-            type: '[TODO] Remove todo',
-            payload: id
-        });
-    }
+	const handleDeleteTodo = (id) => {
+		dispatch({
+			type: '[TODO] Remove todo',
+			payload: id,
+		});
+	};
 
-    const handleToggleTodo = ( id ) => {
-        // console.log({id});
-        dispatch({
-            type: '[TODO] Toggle todo',
-            payload: id
-        });
-    }
+	const handleToggleTodo = (id) => {
+		dispatch({
+			type: '[TODO] Toggle todo',
+			payload: id,
+		});
+	};
 
-    return {
-        todos,
-        handleNewTodo,
-        handleDeleteTodo,
-        handleToggleTodo,
-    }
-}
+	return {
+		todos,
+		handleNewTodo,
+		handleDeleteTodo,
+		handleToggleTodo,
+	};
+};
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -12,9 +12,12 @@ import { Layout } from './05-useLayoutEffect/Layout.jsx';
 import { Memorize } from './06-memos/Memorize.jsx';
 import { MemoHook } from './06-memos/MemoHook.jsx';
 import { CallbackHook } from './06-memos/CallbackHook.jsx';
+import { TodoApp } from './08-useReducer/TodoApp.jsx';
+
+import './08-useReducer/intro-reducer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	// <React.StrictMode>
-	<CallbackHook />
+	<TodoApp />
 	// </React.StrictMode>
 );
