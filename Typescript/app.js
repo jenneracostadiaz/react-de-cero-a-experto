880cfa5b09f6861309ba5c4bab4777e326b9fb54
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2024-08-06 00:37:09 -0500

    Init Typescript Course

---
+++ Typescript/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let msg = 'Hola mundo';
+const hero = {
+    name: 'Ironman',
+    age: 35,
+};
+hero.age = 45;
+console.log(hero.age);
---
+++ Typescript/app.ts
@@ -0,0 +1,9 @@
+let msg: string = 'Hola mundo';
+const hero = {
+	name: 'Ironman',
+	age: 35,
+};
+
+hero.age = 45;
+
+console.log(hero.age);
